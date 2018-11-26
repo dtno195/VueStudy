e344a477b759a2845a1eee2de7f3235efbd91a77
nws-ducnt <ducnt@newwave.vn>
2018-11-26 17:46:45 +0700

    'hoctoibai7'

--- study/udemy-completeguilde/06 VueCLI/my-project/src/main.js
+++ study/udemy-completeguilde/06 VueCLI/my-project/src/main.js
@@ -3,13 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import Home from './Home.vue'
 Vue.config.productionTip = false
-
+Vue.component('app-server', Home)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
