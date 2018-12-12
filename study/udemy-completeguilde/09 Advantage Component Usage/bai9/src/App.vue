86e8b68090853b3c8ef4fd662743cc9394c35898
nws-ducnt <ducnt@newwave.vn>
2018-12-12 15:25:03 +0700

    bai 9

---
+++ study/udemy-completeguilde/09 Advantage Component Usage/bai9/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <appQuoute>
+            <h2>The Quote</h2>
+            <p>A wonderful Quote</p>
+          </appQuoute>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Quoute from "./components/Quote";
+
+export default {
+  name: "app",
+  components: {
+    appQuoute: Quoute
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
