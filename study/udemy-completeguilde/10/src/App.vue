9d1e673fb774f43481cd8d6df0c8101646d35aaf
ducnt <dtno1.study@gmail.com>
2018-12-20 23:11:49 +0700

    start bai 10  11

---
+++ study/udemy-completeguilde/10/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+   
+  </div>
+</template>
+
+<script>
+import Quote from './components/Quote.vue'
+import QuoteGrid from './components/QuoteGrid'
+
+export default {
+  name: 'app',
+  components: {
+      appQuoteGrid: QuoteGrid
+  },
+  data:function(){
+    return {
+      quotes:[
+        'Just a Quote to see something'
+      ],
+      maxQuotes:10
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ study/udemy-completeguilde/10/src/components/QuoteGrid.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="row">
+    <app-quote v-for="quote in quotes" :key="quote.id">{{quotes}}</app-quote>
+  </div>
+</template>
+<script>
+import Quote from "./Quote";
+export default {
+  props: ["quotes"],
+  components: {
+    appQuote: Quote
+  }
+};
+</script>
+<style>
+</style>
+
