499b614e0b8872e73fbf6538622afc02b2be901d
ducnt <dtno1.study@gmail.com>
2019-01-01 16:26:36 +0700

    13 - Mixin : duplicate code  filter

---
+++ study/udemy-completeguilde/13-Mixin-Filter/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h1>Mixins And Filter</h1>
+    <p>{{text|toUppercase|to-lowercase}}</p>
+    <hr>
+    <input v-model="filterText">
+    <ul>
+      <li v-for="fruit in filteredFruits" :key="fruit.id">{{fruit}}</li>
+    </ul>
+    <hr>
+    <app-list></app-list>
+  </div>
+</template>
+
+<script>
+import List from './List'
+import {fruitMixin} from './fruitMixin.js'
+export default {
+  mixins:[fruitMixin],
+  data() {
+    return {
+      text: "hEllO s1mPle",
+    };
+  },
+  filters: {
+    toUppercase(value) {
+      return value.toUpperCase();
+    }
+  },
+  components:{
+    appList : List
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ study/udemy-completeguilde/13-Mixin-Filter/src/List.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Mixins And Filter</h1>
+    <hr>
+    <input v-model="filterText">
+    <ul>
+      <li v-for="fruit in filteredFruits" :key="fruit.id">{{fruit}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import {fruitMixin} from './fruitMixin.js'
+export default {
+ mixins:[fruitMixin]
+};
+</script>
+
+<style>
+</style>
