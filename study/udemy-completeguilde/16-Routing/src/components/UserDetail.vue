abfc71f61974e22a837b107a61c060411550c32d
ducnt <dtno1.study@gmail.com>
2019-01-06 21:15:47 +0700

    end bai 16 ve router . doan phia sau ve guard router thi chua hieu ro cho lam

--- study/udemy-completeguilde/16-Routing/src/components/UserDetail.vue
+++ study/udemy-completeguilde/16-Routing/src/components/UserDetail.vue
@@ -1,18 +1,23 @@
 <template>
-    <div>
-        <h3>Some User Details</h3>
-        <p>The User Loaded has Id : {{$route.params.id}}</p>
-        <router-link tag="button" 
-            :to="{ name : 'userEdit', params:{id: $route.params.id},query:{locale:'vn',q:1000}}"
-            class="btn btn-warning"
-        >Edit User</router-link>
-    </div>
+  <div>
+    <h3>Some User Details</h3>
+    <p>The User Loaded has Id : {{$route.params.id}}</p>
+    <router-link tag="button" :to="link" class="btn btn-warning">Edit User</router-link>
+  </div>
 </template>
 <script>
 export default {
-    
-}
+  data() {
+    return {
+      link: {
+        name: "userEdit",
+        params: { id: this.$route.params.id },
+        query: { locale: "vn", q: 1000 },
+        hash:'#data'
+      }
+    };
+  }
+};
 </script>
 <style>
-
 </style>
--- study/udemy-completeguilde/16-Routing/src/routes.js
+++ study/udemy-completeguilde/16-Routing/src/routes.js
@@ -3,11 +3,24 @@ import UserStart from './components/UserStart.vue'
 import UserDetail from './components/UserDetail.vue'
 import UserEdit from './components/UserEdit.vue'
 import Home from './components/Home.vue'
+import Header from './components/Header.vue'
 export const routes = [
-    {path: '/',component: Home , name:'home'},
-    {path: '/user/',component: User , children:[
+    {path: '/', name:'home', components:{
+        default: Home,
+        'header-top':Header
+    }},
+    {path: '/user/',components:{
+        default: User,
+        'header-bottom':Header
+    } , children:[
         {path:'',component:UserStart},
-        {path:':id',component:UserDetail},
+        {path:':id',component:UserDetail,beforeEnter: (to, from, next) => {
+            // ...
+            console.log('inside router setup');
+            next();
+        }},
         {path:':id/edit',component:UserEdit, name:'userEdit'},
     ]},
+    {path:'/redirect-me',redirect:{name:'home'}},
+    {path:'*' , redirect : '/'}
 ];
